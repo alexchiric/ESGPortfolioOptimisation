02cfe639a8f8fb8e1cda5fd456bd1d0e73627f99
alexchiric <91326811+alexchiric@users.noreply.github.com>
2021-10-01 18:09:10 +0300

    Update ESG.py

--- ESG.py
+++ ESG.py
@@ -11,14 +11,18 @@ from pypfopt.efficient_frontier import EfficientFrontier
 
 mylist = ["MMM", "AXP", "AAPL", "BA", "CAT", "CVX", "CSCO", "KO", "DOW", "XOM",
           "GS", "HD", "INTC", "IBM", "JNJ", "JPM", "MCD", "MRK", "NKE", "PFE",
-          "PG", "RTX", "TRV", "UNH", "VZ", "V", "WBA", "WMT", "DIS"]
+          "PG", "RTX", "TRV", "UNH", "VZ",]
+
+#   "V", "WBA", "WMT", "DIS"]
 
 ESG_list = [75.51, 22.31, 13.68, 78.88, 73.85, 88.39, 3.12, 39.92, 20.00, 77.93,
             40.7, 3.15, 13.78, 8.24, 55.27, 52.24, 36.38, 31.08, 9.86, 39, 48.27,
-            55, 18.04, 23.54, 16.69, 11.41, 18.01, 49.23, 9]
+            55, 18.04, 23.54, 16.69,]
+
+#   11.41, 18.01, 49.23, 9]
 
 
-def ESG_Portf(S, mylist, ESG):
+def ESG_Portf(S, mylist, ESG_list, ESG_bound_U, ESG_bound_L):
     n = len(mylist)
     w = cp.Variable(n)
 
@@ -26,7 +30,8 @@ def ESG_Portf(S, mylist, ESG):
     objective = cp.Minimize(cp.quad_form(w, S))
     constraints = [cp.sum(w) == 1,
                    w >= 0.01,
-                   Portfolio_ESG <= 20, ]
+                   Portfolio_ESG <= ESG_bound_U,
+                   Portfolio_ESG >= ESG_bound_L, ]
 
     prob = cp.Problem(objective, constraints)
 
@@ -54,7 +59,6 @@ def Mean_Variance(S, mylist):
     result = prob.solve(solver=cp.SCS, verbose=False, use_indirect=True)
 
     print("")
-    print("optimal variables= ", w.value)
     print(sum(w.value))
 
     Portfolio_Rating = w.value @ ESG_list
@@ -84,6 +88,7 @@ S = np.array(risk_models.CovarianceShrinkage(Risk_model_PSD).ledoit_wolf())
 
 ef = EfficientFrontier(mu, S, weight_bounds=(0.01, 1))
 
-weights_meanvar = Mean_Variance(S, mylist)
-weights_ESG = ESG_Portf(S, mylist, ESG_arr)
-
+print(Mean_Variance(S, mylist))
+print(ESG_Portf(S, mylist, ESG_arr, ESG_bound_U=21, ESG_bound_L=19))
+print(ESG_Portf(S, mylist, ESG_arr, ESG_bound_U=41, ESG_bound_L=39))
+print(ESG_Portf(S, mylist, ESG_arr, ESG_bound_U=61, ESG_bound_L=59))
